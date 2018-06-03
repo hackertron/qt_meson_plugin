7c7dcf8cf5ce8b60d211b10f396e91116ffb60dc
Jay Gupta <cooljay.gupta@gmail.com>
2018-06-03 17:40:58 +0530

    mesonproject class added

---
+++ mesonproject.cpp
@@ -0,0 +1,6 @@
+#include "mesonproject.h"
+
+MesonProject::MesonProject()
+{
+
+}
---
+++ mesonproject.h
@@ -0,0 +1,11 @@
+#ifndef MESONPROJECT_H
+#define MESONPROJECT_H
+
+
+class MesonProject
+{
+public:
+    MesonProject();
+};
+
+#endif // MESONPROJECT_H
