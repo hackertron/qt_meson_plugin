5954127b905ec0681e675bdfd7e9fd0af1ee7c59
Jay Gupta <cooljay.gupta@gmail.com>
2018-06-03 17:43:29 +0530

    projectExplorer inherited

--- mesonproject.h
+++ mesonproject.h
@@ -1,8 +1,12 @@
 #ifndef MESONPROJECT_H
 #define MESONPROJECT_H
 
+#include <projectexplorer/project.h>
+#include <projectexplorer/projectnodes.h>
 
-class MesonProject
+#include <QDir>
+
+class MesonProject : public ProjectExplorer::Project
 {
 public:
     MesonProject();
