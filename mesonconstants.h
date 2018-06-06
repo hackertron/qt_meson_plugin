e4a6f9b3c25d550b04abfb3d4c296a93514270ae
jayssj11 <cooljay.gupta@gmail.com>
2018-06-06 11:46:52 +0530

    added license header

---
+++ mesonconstants.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace constants {
+const char C_MESONPROJECT_ID[] = "Meson.MesonProject";
+const char C_MESONEDITOR_ID[] = "Meson.MesonEditor";
+const char C_EDITOR_DISPLAY_NAME[] = QT_TRANSLATE_NOOP("OpenWith::Editors", "Meson Editor");
+//const char C_MESON_ICON_PATH[] = ":/images/meson.png";
+
+
+/*******************************************************************************
+ * MIME type
+ ******************************************************************************/
+const char C_MESON_MIMETYPE[] = "text/plain";
+
+
+}
